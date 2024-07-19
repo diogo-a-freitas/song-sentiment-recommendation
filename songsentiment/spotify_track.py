d006934fb16e954018841cf11e213a78f2c157c8
Johannes Roehr <jroehr@posteo.net>
2024-07-19 11:37:18 +0200

    Fixed spotify bug

--- songsentiment/spotify_track.py
+++ songsentiment/spotify_track.py
@@ -28,10 +28,13 @@ class SpotifyApiExtractor:
             q="artist:" + artist + " track:" + title,
             type="track",
             limit=1)
-        print(self.ta_response)
+        #print(self.ta_response)
 
         # parse attributes from track
-        self.track = self.ta_response['tracks']['items'][0]
+        if len(self.ta_response['tracks']['items']) == 0:
+            return None
+        else:
+            self.track = self.ta_response['tracks']['items'][0]
 
         self.track_id = self.track['id']
         self.track_name = self.track['name']
@@ -49,16 +52,20 @@ class SpotifyApiExtractor:
 
     def get_tracks_and_artists(self, titles, artists):
 
-        song_attrs = []
+        songs_attrs = []
 
         for title, artist in zip(titles, artists):
-            song_attrs.append(self.get_track_base_attrs(title, artist))
 
-        time.sleep(1)
+            attrs = self.get_track_base_attrs(title, artist)
 
-        return pd.DataFrame(song_attrs)
+            if attrs is not None:
+                songs_attrs.append(attrs)
+
+        #time.sleep(1)
+
+        return pd.DataFrame(songs_attrs, columns=['Track', 'Artists', 'ID', 'Danceability', 'Energy', 'Valence', 'Tempo'])
 
 
-print(SpotifyApiExtractor().get_track_base_attrs('Gasoline', 'The Weeknd'))
+#print(SpotifyApiExtractor().get_track_base_attrs('Gasoline', 'The Weeknd'))
 
-print(SpotifyApiExtractor().get_tracks_and_artists(['Gasoline', 'Espresso'], ['The Weeknd', 'Sabrina Carpenter']))
+#print(SpotifyApiExtractor().get_tracks_and_artists(['Gasoline', 'Espresso'], ['The Weeknd', 'Sabrina Carpenter']))

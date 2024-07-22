6b11282e73b2f0ea5044d081ef124d4d3eb1ba00
Luis Mockler <luis.mockler@gmail.com>
2024-07-22 10:25:11 +0100

    changes

--- api/app.py
+++ api/app.py
@@ -17,43 +17,41 @@ params = dict(
     text = utext
 )
 
-
 if len(utext) < 20:
-        st.markdown('Please input more than 20 characters')
+    st.markdown('Please input more than 20 characters')
 
 elif len(utext) > 140:
-        st.markdown('Please input less than 140 characters')
+    st.markdown('Please input less than 140 characters')
 
 else:
-        '''---'''
+    '''---'''
 
-        res = requests.get(url, params).json()
+    res = requests.get(url, params).json()
 
-        st.markdown('Sentiment: *' + str(res['user_sentiment']['label']) +
+    st.markdown('Sentiment: *' + str(res['user_sentiment']['label']) +
                         '* with a score of ' + str(round(res['user_sentiment']['score'], 2)))
 
-        st.markdown('Topics: ' + ', '.join(res['user_topics']))
+    st.markdown('Topics: ' + ', '.join(res['user_topics']))
 
-        """
-        ---
-        Our recommendation for you:
-        """
+    """
+    ---Our recommendation for you:
+    """
+    #res["songs"] = res["songs"].sort_values(by="valence", ascending=False)
+    for song in res['songs']:
 
-        for song in res['songs']:
+        if res['user_sentiment']['label'] == song['sentiment_label']: #and song["cluster"] == 0:
 
-            if res['user_sentiment']['label'] == song['sentiment_label']:
+            url = f"https://open.spotify.com/embed/track/{song['id']}?utm_source=generator"
 
-                url = f"https://open.spotify.com/embed/track/{song['id']}?utm_source=generator"
+            components.iframe(url, height=80)
 
-                components.iframe(url, height=80)
+            st.markdown('Song cluster: ' + str(song['cluster']))
+            st.markdown('Song valence: ' + str(song['valence']))
+            st.markdown('Song Sentiment Score: ' + str(song['sentiment_score']))
+            st.markdown('Song Sentiment Score: ' + str(song['sentiment_label']))
 
-                st.markdown('Song cluster: ' + str(song['cluster']))
-                st.markdown('Song valence: ' + str(song['valence']))
-                st.markdown('Song Sentiment Score: ' + str(song['sentiment_score']))
-                st.markdown('Song Sentiment Score: ' + str(song['sentiment_label']))
+        #elif res['user_sentiment']['label'] == song['sentiment_label']:
+            #pass
 
-            elif res['user_sentiment']['label'] == song['sentiment_label']:
-                pass
-
-            else:
-                pass
+        else:
+            pass
